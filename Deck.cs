edbf04b042899555ce42f86a70b3a7c0207983db
JustinCM1 <104384617+JustinCM1@users.noreply.github.com>
2022-10-06 21:15:08 -0600

    finished HiLo

--- Deck.cs
+++ Deck.cs
@@ -1,91 +1,14 @@
 using System;
-using System.Collections.Generic;
-
 namespace hiloGame
 {
-    public class Director
+    class Card
     {
-        List<Card> cards = new List<Card>();
-        bool isPlaying = true;
-        int winningBonus = 100;
-        int wrongGuess = 75;
-        int totalScore = 300;
+        public int cardValue;
 
-        int currentCard;
-        int nextCard;
-        public Director()
-        {
-            for (int i = 0; i < 1; i++){
-                Card card = new Card();
-                cards.Add(card);
-
-            }
-        }
-
-        ///Starts hilo game,
-        public void StartGame()
-        {
-            foreach (Card card in cards){
-                card.getNewCard();
-                currentCard = card.cardValue;
-            }
-            while (isPlaying)
-            {
-                MainGame();
-                GameCheck();
-            }
-        }
-
-        public void MainGame(){
-            Console.WriteLine($"The card is {currentCard}");
-            if (!isPlaying)
-            {
-                return;
-            }
-
-            foreach (Card card in cards){
-                card.getNewCard();
-                nextCard = card.cardValue;
-            }
-            Console.Write("Higher or Lower: [h/l]  ");
-            string cardGuess = Console.ReadLine();
-            Console.WriteLine($"The next card was:{nextCard}");
-            if (cardGuess.Equals("h") && currentCard < nextCard){
-                totalScore += winningBonus;
-            }
-            else if(cardGuess.Equals("l") && currentCard > nextCard){
-                totalScore += winningBonus;
-            }
-            else if(cardGuess.Equals("h") && currentCard > nextCard){
-                totalScore -= wrongGuess;
-                if (totalScore < 0){
-                    totalScore = 0;
-                }
-            }
-            else if(cardGuess.Equals("l") && currentCard < nextCard){
-                totalScore -= wrongGuess;
-                if (totalScore < 0){
-                    totalScore = 0;
-                }
-            }
-
-        }
-
-        /// See if the player can keep playing from their score
-        public void GameCheck(){
-            Console.WriteLine($"Your score is: {totalScore}");
-            if (totalScore == 0){
-                isPlaying = false;
-            }
-            if (!isPlaying)
-            {
-                return;
-            }
-
-            currentCard = nextCard;
-            Console.Write("Keep Playing? [y/n] ");
-            string rollDice = Console.ReadLine();
-            isPlaying = (rollDice == "y");
+        /// Make a class that pulls a random card
+        public void getNewCard(){
+            Random randomGenerator = new Random();
+            cardValue = randomGenerator.Next(1,13);
         }
     }
 }
